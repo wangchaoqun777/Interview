17c632e6afbaabd270feede562cdee16715ab862
1522688684@qq.com <1522688684@qq.com>
2021-08-12 17:03:21 +0800

    fix: 更新目录

--- my_cnode/src/reducers/details.js
+++
@@ -1,6 +0,0 @@
-export default function detail( state={},action ) {
-  switch(action.type) {
-    default: 
-      return state
-  }
-}
--- my_cnode/src/view/main-footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Layout } from 'antd'
-export default class MainFooter extends Component {
-  render () {
-    return (<Layout.Footer style={{textAlign:"center"}}>
-      这是一个底部
-      </Layout.Footer>)
-  }
-}
